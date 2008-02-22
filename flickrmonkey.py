decbc2ea33b91118d93ac5abe184f133eb4a2d7e
paulproteus <paulproteus@committer.creativecommons.org>
2008-02-22 22:41:09 +0000

    one more lousy piece of XML hackery

--- flickrmonkey.py
+++ flickrmonkey.py
@@ -1,30 +1,41 @@
 # Do hilarious monkey patching to work around Flickr bad Unicode
 import xml.dom.expatbuilder
+import xml.dom.minidom
 import xml.parsers.expat
 import BeautifulSoup
 
 old_parseString = xml.dom.expatbuilder.ExpatBuilder.parseString
 
+def is_valid_xml(maybe_valid):
+    try:
+        parsed = xml.dom.minidom.parseString(maybe_valid)
+        return True
+    except xml.parsers.expat.ExpatError:
+        return False
+
 def new_parseString(self, string):
-    print "FYI, Asheesh's parseString"
-    soup = BeautifulSoup.BeautifulStoneSoup(string)
-    soup_hacked_string = str(soup)
+    if is_valid_xml(string):
+        return old_parseString(self, string)
+    else:
+        print "FYI, Asheesh's parseString"
+        soup = BeautifulSoup.BeautifulStoneSoup(string)
+        soup_hacked_string = str(soup)
 
-    # post soup, no need to fix unicode    
-    
+        # post soup, no need to fix unicode    
+        # But some attribs might have evil ampersand nonsense
+        # Can't do this in a try-except because the parser object
+        # maintains state, so you only get one shot.
+
+        # Fix up attribs on photo elements
+        if not is_valid_xml(soup_hacked_string):
+            for photo in soup('photo'):
+                for key in photo.attrMap:
+                    if '&' in photo[key]:
+                        cleaned = photo[key].replace('&', '&amp;')
+                        photo[key] = cleaned
+                        print 'FYI, Asheesh cleaned', key, 'in', photo['id']
+            soup_hacked_string = str(soup)
+        
     return old_parseString(self, soup_hacked_string)
-#    except xml.parsers.expat.ExpatError, e:
-#        print "FYI, Asheesh's workaround"
-#        lines = string.split('\n')
-#        lines = [thing.strip() for thing in lines]
-
-        # Have faith that <note>s are the problem
-#        line = lines[e.lineno - 1].strip()
-#        assert (line.startswith('<note') and line.endswith('</note>')) # screw it
-#        non_note_lines = [line for line in thing if not (line.startswith('<note>') and line.endwith('</note>'))]
-#        fixed_string = '\n'.join(non_note_lines)
-        #fixed_string = unicode(string, 'utf-8', 'replace').encode('utf-8')
-#        return old_parseString(self, fixed_string)
 
 xml.dom.expatbuilder.ExpatBuilder.parseString = new_parseString
-
